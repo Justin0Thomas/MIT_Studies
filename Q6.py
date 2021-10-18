004be6ace24977df65a96f9f0fec30def345a902
Justin0Thomas <92721831+Justin0Thomas@users.noreply.github.com>
2021-10-18 19:53:24 +1000

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,23 @@
+# Justin Thomas MIT211573
+# Week 4 Workshop Question 6
+# Defines a class named rectangle that has a method
+# to compute the area of the rectangle
+# Define the class
+class Rectangle():
+    # 
+    def __init__(self, len, wid):
+        # Set the length and width as instance parameters
+        self.length = len
+        self.width  = wid
+
+    # Function to calculate the area    
+    def rectangle_area(self):
+        return self.length*self.width
+    
+# Prompt the user for the length of the sides
+len = float(input("Enter the rectangle length: "))
+wid = float(input("Enter the rectangle width: "))
+# Run the variables
+InputRectangle = Rectangle(len, wid)
+# Output the calculated area
+print(InputRectangle.rectangle_area())
